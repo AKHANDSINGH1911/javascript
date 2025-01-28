e2f525901c7a9b97f6be896cd69953a3379dc554
AKHANDSINGH1911 <150822137+AKHANDSINGH1911@users.noreply.github.com>
2025-01-28 18:25:17 +0000

    update stack and heap

--- 01_basic/05_datatype2.js
+++ 01_basic/05_datatype2.js
@@ -37,6 +37,34 @@ const myFunction = function(){
 }
 console.log(typeof myFunction);  //function
 
-
-
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// summary 
+// 1)datatype : primitive and non primitive(reference)
+// 2)memory: stack and heap memory
+// stack used in primitive datatype( we get copy )
+// heap used in  non primitive therfore we get refernce of original value
+//if we change in refernce original wiill be change
+//example of  primitive used in stack we get copy
+let myname="akhand"
+let anothername=myname
+anothername="akhandsingh"  //change in copy
+console.log(myname)           //no output change in original
+console.log(anothername)    //name changes 
+
+//exampe of non primitive in heap we get reference from userone stored in heap
+let userone={
+    email:"qwertyuiop",     //remember comma is imp otherwise errror
+    upi:"asdfghjkl"      //ALSO : THIS sign is imp in object while assinging value
+}
+let usertwo=userone
+// usertwo="zxcvbnm"   //wrong way to access object
+//use dot operator to get access object(email hai yha object)
+usertwo.email="zxcvbnm"
+// console.log(usertwo)  //zxcvbnm
+console.log(userone)  //{ email: 'qwertyuiop', upi: 'asdfghjkl' } ye ouput hoga
+//to get ouput of object
+console.log(userone.email)   //zxcvbnm
+console.log(usertwo.email)   //zxcvbnm
+
+
