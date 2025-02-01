bd3a8101e80bfe4319ca4a8006bc669b4de23aa4
AKHANDSINGH1911 <150822137+AKHANDSINGH1911@users.noreply.github.com>
2025-02-01 20:34:06 +0000

    revision of 1

---
+++ revision.js/1.js
@@ -0,0 +1,28 @@
+// let,var,const
+
+//console.log() for output
+//console.table() for output in form of table
+
+
+
+/*conversion*/
+// let score ="33"
+// console.log(typeof score)  //string
+// let value=Number(score)              //Number conversion for int
+// console.log(typeof value)   // number
+
+/*comparison*/
+// console.log("2" > 1);   //true
+// console.log("02" > 1);   //true
+// console.log(null > 0);   //falSE
+// console.log(null == 0);   //false
+// console.log(null >= 0);    //true
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0);  // false
+// console.log("2" === 2);   //false
+
+/* datatype*/
+/*string*/
+// both learn from orginal notes
+
